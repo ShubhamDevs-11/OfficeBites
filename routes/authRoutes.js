fa4fbdfbc3231c906081db466648f5b68c07a4ad
ShubhamDevs-11 <shubhamkapadia19@gmail.com>
2026-06-21 23:55:19 +0530

    fix: imports

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { register, login, logout } = require("../controllers/auth.controller");
+const { register, login, logout } = require("../controllers/authController");
 
 router.post("/register", register);
 router.post("/login", login);
